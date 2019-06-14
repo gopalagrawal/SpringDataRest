a1bf1d7233b96f84d88b1616ec524929bc05684f
ga <ga.willbe@gmail.com>
2019-06-14 09:27:32 +0530

    First Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into alien values (10, 'gopal', 	'python');
+insert into alien values (20, 'shweta', 'fashion');
+insert into alien values (30, 'monica', 'teaching');
+insert into alien values (40, 'navin', 	'java');
+insert into alien values (50, 'gita', 	'c++');
+insert into alien values (60, 'girija', 'dancing');
+insert into alien values (70, 'Gordon', 'java');
